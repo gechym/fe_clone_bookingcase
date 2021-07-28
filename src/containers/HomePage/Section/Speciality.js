97ff3037c595b8374953ebc4011b41cbfd3dcdbb
gechym <nguyenducbao166@gmail.com>
2021-07-28 20:53:41 +0700

    css slider

--- src/containers/HomePage/Section/Speciality.js
+++ src/containers/HomePage/Section/Speciality.js
@@ -7,41 +7,58 @@ import './Speciality.scss'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-
+import specialityImg from "../../../assets/speciality/co-xuong-khop.jpg"
+                        
 
 class Speciality extends Component {
     render() {
         var settings = {
-            dots: false,
             infinite: true,
             speed: 500,
             slidesToShow: 4,
             slidesToScroll: 1,
+            // autoplay: true,
+            // speed: 2000,
+            // autoplaySpeed: 2000,
+            // cssEase: "linear"
           };
         return (
             <div className="section-specialitry">
-                <div className="speciality-content">
-                    <Slider {...settings}>
-                        <div className="img-customize">
-                            <h1>1</h1>
-                        </div>
-                        <div className="img-customize">
-                            <h1>2</h1>
-                        </div>
-                        <div className="img-customize">
-                            <h1>3</h1>
-                        </div>
-                        <div className="img-customize">
-                            <h1>4</h1>
-                        </div>
-                        <div className="img-customize">
-                            <h1>5</h1>
-                        </div>
-                        <div className="img-customize">
-                            <h1>6</h1>
-                        </div>
-                    </Slider>
+                <div className="speciality-container">
+                    
+                    <div className="speciality-header">
+                        <span className="title-section">Chuyên khoa phổ biến</span>
+                        <button className="button-section">Xem thêm</button>
+                    </div>
+
+                    <div className="speciality-body">
+                        <Slider {...settings}>
+                            <div className="speciality-customize">
+                                <div className="bg-img"></div>
+                                <div>Cơ xương khớp 1</div>
+                            </div>
+                            <div className="speciality-customize">
+                                <div className="bg-img"></div>
+                                <div>Cơ xương khớp 2</div>
+                            </div>
+                            <div className="speciality-customize">
+                                <div className="bg-img"></div>
+                                <div>Cơ xương khớp 3</div>
+                            </div>
+                            <div className="speciality-customize">
+                                <div className="bg-img"></div>
+                                <div>Cơ xương khớp 4</div>
+                            </div>
+                            <div className="speciality-customize">
+                                <div className="bg-img"></div>
+                                <div>Cơ xương khớp 5</div>
+                            </div>
+                            <div className="speciality-customize">
+                                <div className="bg-img"></div>
+                                <div>Cơ xương khớp 6</div>
+                            </div>
+                        </Slider>
+                    </div>
                 </div>
             </div>
         )
